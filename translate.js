c08019ecff41b161500a97d9884f81401f6ae024
Stephen Wong <stwong1@gmail.com>
2018-07-27 12:07:29 -0700

    working example with static input

---
+++ translate.js
@@ -0,0 +1,24 @@
+function postAjax(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('POST', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+// example request
+function translateS() {
+    postAjax('https://translation.googleapis.com/language/translate/v2', 'key=AIzaSyAu5F3tkRzpG-IDmeMMamVs4lu7nsLNk4o&q=cheese&target=es', function(data){ console.log(data); document.getElementById('translateResult').innerHTML = data;});
+}
+
+
+// example request with data object
+// postAjax('http://foo.bar/', { p1: 1, p2: 'Hello World' }, function(data){ console.log(data); });
